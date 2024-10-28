a6e1b58af509a79ef233295395b1e3ced662a080
Admin <cezar.strugariu@neptune-software.com>
2024-10-28 09:54:22 +0000

    Placeholder apikey

--- artifacts/Application/offline_example_app/event/sap.m.Button/ButtonCacheWeather/press.js
+++ artifacts/Application/offline_example_app/event/sap.m.Button/ButtonCacheWeather/press.js
@@ -1,5 +1,5 @@
 let options = {
-    key: "a314e15f4f9422fc0eb01e0281fd59fc", // Required - In Path
+    key: "yourapikey", // Required - In Path
     lat: Math.round(latitude), // Required - In Path
     lon:  Math.round(longitude), // Required - In Path
     units: "metric",
